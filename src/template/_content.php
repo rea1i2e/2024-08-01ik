58c05e483e321c63168216269a05a31ae950b5e5
Yoshiaki Numajiri <afmaar128@gmail.com>
2024-09-09 13:20:27 +0900

    不要ファイル削除

--- src/template/_content.php
+++
@@ -1,5 +0,0 @@
-<div class="main-content-wrapper container">
-	<div class="main-content">
-		<?php include('_form.php'); ?>
-	</div>
-</div>
--- src/template/_error-content.php
+++
@@ -1,7 +0,0 @@
-<section class="error-section container">
-  <div style="background-color:#fff; padding: 16px; font-size: 16px; border: 1px solid #ccc; margin: 64px 16px 256px 16px; opacity: 0.8">
-    <h1 style="color: red; font-size: 24px;">送信に失敗しました。</h1>
-    <div style="margin-top: 16px">お手数をおかけして大変申し訳ございませんが、再度入力のうえ送信をお願いいたします。</div>
-    <div style="margin-top: 16px">再度の送信でも失敗する場合は info@ilhope.co.jp までお問い合わせください。</div>
-  </div>
-</section>
